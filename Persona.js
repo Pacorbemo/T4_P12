18d72a2828aa11a629e7668eb9fcaf1442aad4ac
Francisco <pacorbemo@gmail.com>
2023-11-30 21:35:26 +0100

    Crear clase Persona

---
+++ Persona.js
@@ -0,0 +1,58 @@
+export class Persona {
+    name;
+    surname;
+    email;
+    img;
+    phone;
+    city;
+    country;
+    constructor(name, surname, email){
+        this.name = name;
+        this.surname = surname
+        this.email = email;
+    }
+
+    set name(name){
+        this.name = name
+    }
+    set surname(surname){
+        this.surname = surname
+    }
+    set email(email){
+        this.email = email
+    }
+    set img(img){
+        this.img = img
+    }
+    set phone(phone){
+        this.phone = phone
+    }
+    set city(city){
+        this.city = city
+    }
+    set country(country){
+        this.country = country
+    }
+    
+    get name(){
+        return this.name;
+    }
+    get surname(){
+        return this.surname;
+    }
+    get email(){
+        return this.email;
+    }
+    get img(){
+        return this.img;
+    }
+    get phone(){
+        return this.phone;
+    }
+    get city(){
+        return this.city;
+    }
+    get country(){
+        return this.country;
+    }
+}
